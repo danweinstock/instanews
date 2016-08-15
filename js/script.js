97daf373e3596d7e4bb9ce6b03169455966b4c5a
danweinstock <dan.weinstock@yahoo.com>
2016-08-15 13:22:54 -0400

    individual blurb hover achieved and margin bottom for landing added

--- js/script.js
+++ js/script.js
@@ -7,35 +7,39 @@ $(document).ready(function(){
 
 			console.log(this.value);
 		var url ='http://api.nytimes.com/svc/topstories/v2/'+this.value+'.json?api-key=b1366b81c4e6416d9dcc93e5b4687481';
+
 	$.getJSON(url).done(function(news){
-		console.log(news.results[0])
 		var original = $('.original')
-		// console.log(news.results[0]);
 		$.each(news.results, function(key,value){
-			// console.log(value);
 			var abstract = (value.abstract)
-			var articleURL = (value.url);
-			
+			var articleURL = (value.url);	
 			var clone = original.clone();
-				// console.log(articleURL)
-				// console.log(photoURL);
-		if (typeof(value.multimedia[4].url) !== undefined) {
+		
+		if (typeof(value.multimedia[0]) != undefined && value.multimedia.length != 0) {
 			var photoURL = (value.multimedia[4].url);
 			clone.children(".abstract").text(abstract)
 			clone.children(".pic").append('<a href= "'+articleURL+'" >  <img src="'+ photoURL +'"> </a>')
 		}
-
-			$('.abstract').hide();
-			$('.pic').hover(function(){
-				$('.abstract').show();
-			})
-			$('.pic').mouseleave(function(){
-				$('.abstract').hide()
-			})
+		else{
+			return
+		} 
+			
 			original.remove()
-			$('.overlay').append(clone)
+			// var i = 0;
+			// $('.original').each(function(i){
+			// 	$(this).attr('id',"item" +(i+1));
+			// })
+			// $('.pic').hover(function(){
+			// 	$('.abstract').show();
+			// })
+			if($('.original').length < 12){
+				$('.overlay').append(clone)	
+			}
+			// $('.abstract').hide();
+			// $('.pic').mouseleave(function(){
+			// 	$('.abstract').hide()
+			// })
 		});
 	})
-		});
+		});	
 });
-
