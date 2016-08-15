57f74cf25eed62ff801238cd009ece969e9529b4
danweinstock <dan.weinstock@yahoo.com>
2016-08-15 13:25:59 -0400

    added secure protocol to api key

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			$('.landing').removeClass("full-height").addClass("change-height")
 
 			console.log(this.value);
-		var url ='http://api.nytimes.com/svc/topstories/v2/'+this.value+'.json?api-key=b1366b81c4e6416d9dcc93e5b4687481';
+		var url ='https://api.nytimes.com/svc/topstories/v2/'+this.value+'.json?api-key=b1366b81c4e6416d9dcc93e5b4687481';
 
 	$.getJSON(url).done(function(news){
 		var original = $('.original')
